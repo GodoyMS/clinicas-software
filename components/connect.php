e2f0e3c30a5a8ef07503f6764ac0b758a1237e07
GodoyMS <youremail@domain.com>
2023-01-26 17:26:48 -0500

    5 commit

--- components/connect.php
+++ components/connect.php
@@ -1,6 +1,8 @@
+<!-- FOR PRODUCTION -->
+
 <?php
 
-
+/*
 try{
     $conn = new PDO("mysql:host=localhost;dbname=software_clinicas;port=3307","root","");
     
@@ -9,6 +11,25 @@ try{
     echo $error_message;
     
 }
+*/
 ?>
+
+
+
+<!-- FOR DEPLOYMENT -->
+<?php
+
+
+try{
+    $conn = new PDO("mysql:host=localhost;dbname=u268124124_alpha_clinicas;port=3307","u268124124_ac_user","Crystalcave31!");
+    
+} catch(PDOException $err){
+    $error_message = $err->getMessage();
+    echo $error_message;
+    
+}
+
+?>
+
 
 

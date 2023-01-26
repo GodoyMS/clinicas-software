250c9b754c6ad2f47a94e4f3b5090d48ada70a6e
GodoyMS <youremail@domain.com>
2023-01-26 17:23:27 -0500

    4 commit

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ if(isset($_SESSION['user_id'])){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Alpha Clinicas XDDDD</title>
+    <title>Alpha Clinicas </title>
     <link rel="stylesheet" href="https://unpkg.com/flowbite@1.6.0/dist/flowbite.min.css" />
     <link rel="icon" type="image/x-icon" href="images/imgLogo/logo-no-back.png">
 

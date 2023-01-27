d16726eb22b938cc0594fe16b530270dfe3fadc3
GodoyMS <youremail@domain.com>
2023-01-26 21:28:01 -0500

    26 commit

--- index.php
+++ index.php
@@ -36,7 +36,7 @@ if(isset($_SESSION['user_id'])){
             <h1 class="max-w-2xl mb-4 text-2xl md:text-5xl font-extrabold flex justify-center tracking-tight leading-none ">Sistematiza tu consultorio y administra los procesos eficazmente </h1>
             <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">Software para gestión de información, pagos, citas, tratamientos, historial médico y mas.</p>
             <div class="flex justify-center ">
-            <a href="registrarse.php" class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-blue-600 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
+            <a href="clinicas/registrarse.php" class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-blue-600 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
                 Registra tu consultorio Ahora 
                 <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
             </a>

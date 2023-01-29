0fec9dff82aa01ad5b6e8b0dddd4e1194ff3775b
GodoyMS <youremail@domain.com>
2023-01-29 02:08:58 -0500

    33 commit

--- usuarios/dashboard.php
+++ usuarios/dashboard.php
@@ -23,13 +23,13 @@
             
             $updateInhabilitadoState= $conn->prepare("UPDATE `eventos` SET estado = 'Confirmado' WHERE idPaciente = ? AND inicio = ? ");
             $updateInhabilitadoState->execute([$userPaciente_id,$citaFechaInicio]);          
-            echo'<div id="toast-success" class="mx-auto relative flex justify-center mt-4 flex items-center w-full max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800" role="alert">
-            <div class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-green-500 bg-green-100 rounded-lg dark:bg-green-800 dark:text-green-200">
+            echo'<div id="toast-success" class="mx-auto relative flex justify-center mt-4 flex items-center w-full max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow " role="alert">
+            <div class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-green-500 bg-green-100 rounded-lg ">
                 <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
                 <span class="sr-only">Check icon</span>
             </div>
             <div class="ml-3 text-sm font-normal">Gracias por tu confirmación</div>
-            <button type="button" class="ml-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700" data-dismiss-target="#toast-success" aria-label="Close">
+            <button type="button" class="ml-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex h-8 w-8  " data-dismiss-target="#toast-success" aria-label="Close">
                 <span class="sr-only">Close</span>
                 <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
             </button>
@@ -44,12 +44,12 @@
             
             $updateInhabilitadoStateCancelar= $conn->prepare("UPDATE `eventos` SET estado = 'Cancelado' WHERE idPaciente = ? AND inicio = ? ");
             $updateInhabilitadoStateCancelar->execute([$userPaciente_id,$citaFechaInicio]);          
-            echo'<div id="alert-border-3" style="max-width:500px" class=" mx-auto flex p-4 mb-4 text-green-700 bg-green-100 border-t-4 border-green-500 dark:text-green-400 dark:bg-gray-800" role="alert">
+            echo'<div id="alert-border-3" style="max-width:500px" class=" mx-auto flex p-4 mb-4 text-green-700 bg-green-100 border-t-4 border-green-500 " role="alert">
             <svg class="flex-shrink-0 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path></svg>
             <div class="ml-3 text-sm font-medium">
               Cita cancelada. Lamentamos tu decisión. Recuerda que puedes solicitar otra cita en otra fecha y horario disponible
             </div>
-            <button type="button" class="ml-auto -mx-1.5 -my-1.5 bg-green-100 text-green-500 rounded-lg focus:ring-2 focus:ring-green-400 p-1.5 hover:bg-green-200 inline-flex h-8 w-8 dark:bg-gray-800 dark:text-green-300 dark:hover:bg-gray-700"  data-dismiss-target="#alert-border-3" aria-label="Close">
+            <button type="button" class="ml-auto -mx-1.5 -my-1.5 bg-green-100 text-green-500 rounded-lg focus:ring-2 focus:ring-green-400 p-1.5 hover:bg-green-200 inline-flex h-8 w-8 "  data-dismiss-target="#alert-border-3" aria-label="Close">
               <span class="sr-only">Dismiss</span>
               <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
             </button>
@@ -334,8 +334,8 @@ body {
 <span class="block w-60 mx-auto h-1 rounded-full bg-blue-500"></span>
 
 <div class="p-4 shadow-lg relative overflow-x-auto shadow-md sm:rounded-lg">
-    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+    <table class="w-full text-sm text-left text-gray-500 ">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50 ">
             <tr>
                 <th scope="col" class="px-6 py-3">
                     Doctor
@@ -363,8 +363,8 @@ body {
           
 
             ?>
-            <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
-                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+            <tr class="bg-white border-b ">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap ">
                     <?=$fetchCitaPendienteTodo['doctor'];?>
                 </th>
                 <td class="px-6 py-4">
@@ -382,16 +382,16 @@ body {
                 </td>
                 <?php
                 if($fetchCitaPendienteTodo['estado']=="Por confirmar"){
-                    echo '<td class="  px-6 py-4"><span class="px-2 py-1  leading-tight text-gray-800 bg-gray-100 rounded-full dark:bg-green-700 dark:text-green-100">'.$fetchCitaPendienteTodo["estado"].'</span></td>';
+                    echo '<td class="  px-6 py-4"><span class="px-2 py-1  leading-tight text-gray-800 bg-gray-100 rounded-full ">'.$fetchCitaPendienteTodo["estado"].'</span></td>';
                 };
                 if($fetchCitaPendienteTodo['estado']=="Confirmado"){
-                    echo '<td class="  px-6 py-4"><span class="px-2 py-1  leading-tight text-green-800 bg-green-200 rounded-full dark:bg-green-700 dark:text-green-100">'.$fetchCitaPendienteTodo["estado"].'</span></td>';
+                    echo '<td class="  px-6 py-4"><span class="px-2 py-1  leading-tight text-green-800 bg-green-200 rounded-full ">'.$fetchCitaPendienteTodo["estado"].'</span></td>';
                 };
                 if($fetchCitaPendienteTodo['estado']=="Cancelado"){
-                    echo '<td class="  px-6 py-4"><span class="px-2 py-1  leading-tight text-red-800 bg-red-200 rounded-full dark:bg-green-700 dark:text-green-100">'.$fetchCitaPendienteTodo["estado"].'</span></td>';
+                    echo '<td class="  px-6 py-4"><span class="px-2 py-1  leading-tight text-red-800 bg-red-200 rounded-full ">'.$fetchCitaPendienteTodo["estado"].'</span></td>';
                 };
                 if($fetchCitaPendienteTodo['estado']=="Solicitud postergación"){
-                    echo '<td class="  px-6 py-4"><span class="px-2 py-1  leading-tight text-yellow-800 bg-yellow-200 rounded-full dark:bg-green-700 dark:text-green-100">'.$fetchCitaPendienteTodo["estado"].'</span></td>';
+                    echo '<td class="  px-6 py-4"><span class="px-2 py-1  leading-tight text-yellow-800 bg-yellow-200 rounded-full ">'.$fetchCitaPendienteTodo["estado"].'</span></td>';
                 };
                 
                 ?>
